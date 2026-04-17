9c891c5f38fb2c45ff92bd36709826c9d8e35dee
Sandesh Kandel <sandeshkandel2025@gmail.com>
2026-04-17 11:58:32 +1000

    Add file reading and writing functions for the first question

--- question1/encrypt.py
+++ question1/encrypt.py
@@ -1,1 +1,17 @@
+def read_file():
+    with open("raw_text.txt", "r") as f:
+        return f.read()
 
+
+def write_file(filename, content):
+    with open(filename, "w") as f:
+        f.write(content)
+
+
+def main():
+    text = read_file()
+    print(text)  # just to check
+
+
+if __name__ == "__main__":
+    main()
