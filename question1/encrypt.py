4e84430c9601f3bf81d043b5df912d1ed16e0aac
Sandesh Kandel <sandeshkandel2025@gmail.com>
2026-04-17 12:13:09 +1000

    Implement encryption logic for lowercase and uppercase characters

--- question1/encrypt.py
+++ question1/encrypt.py
@@ -10,8 +10,38 @@ def write_file(filename, content):
 
 def main():
     text = read_file()
-    print(text)  # just to check
+
+    shift1 = int(input("Enter shift1: "))
+    shift2 = int(input("Enter shift2: "))
+
+    encrypted = ""
+    for c in text:
+        encrypted += encrypt_char(c, shift1, shift2)
+
+    print("Encrypted text:")
+    print(encrypted)
 
 
+
+def encrypt_char(c, shift1, shift2):
+    if c.islower():
+        if 'a' <= c <= 'm':
+            shift = shift1 * shift2
+            return chr((ord(c) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            shift = shift1 + shift2
+            return chr((ord(c) - ord('a') - shift) % 26 + ord('a'))
+
+    elif c.isupper():
+        if 'A' <= c <= 'M':
+            shift = shift1
+            return chr((ord(c) - ord('A') - shift) % 26 + ord('A'))
+        else:
+            shift = shift2 ** 2
+            return chr((ord(c) - ord('A') + shift) % 26 + ord('A'))
+
+    else:
+        return c
 if __name__ == "__main__":
     main()
+    
